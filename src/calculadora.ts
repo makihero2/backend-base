e8ce9b023e97330195414f9fb13536fecc07acaf
Juan <jhernandezd4d@gmail.com>
2024-08-16 10:13:27 -0400

    se agrega el resto de ecuaciones

--- src/calculadora.ts
+++ src/calculadora.ts
@@ -23,7 +23,7 @@ function suma(a: number, b: number) {
 function restar(a: number, b: number) {
     if (a === undefined || b === undefined) {
         console.log("retornando throw")
-        throw new Error("No se puede sumar indefinidos");
+        throw new Error("No se puede restar indefinidos");
     }
 
     if (typeof a !== 'number' || typeof b !== 'number') {
@@ -33,4 +33,66 @@ function restar(a: number, b: number) {
     return a - b;
 }
 
-export { suma, operar, restar };
+function multiplicar(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        console.log("retornando throw")
+        throw new Error("No se puede multiplicar indefinidos");
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    return a * b;
+}
+
+function dividir(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        console.log("retornando throw")
+        throw new Error("No se puede dividir indefinidos");
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    if (b === 0) {
+        console.log("retornando throw")
+        throw new Error("No se puede dividir por cero");
+    }
+
+    return a / b;
+}
+
+function potencia(base: number, exponente: number) {
+    if (base === undefined || exponente === undefined) {
+        console.log("retornando throw");
+        throw new Error("No se puede calcular la potencia de indefinidos");
+    }
+
+    if (typeof base !== 'number' || typeof exponente !== 'number') {
+        return NaN;
+    }
+
+    return base ** exponente;
+}
+
+function factorial(n: number): number {
+    if (n === undefined) {
+        console.log("retornando throw");
+        throw new Error("No se puede calcular el factorial de indefinidos");
+    }
+
+    if (typeof n !== 'number' || n < 0 || !Number.isInteger(n)) {
+        return NaN; // Factorial sólo está definido para enteros no negativos.
+    }
+
+    if (n === 0 || n === 1) {
+        return 1; // Factorial de 0 o 1 es 1.
+    }
+
+    return n * factorial(n - 1);
+}
+
+
+export { suma, restar, multiplicar, dividir, potencia, factorial, operar };
